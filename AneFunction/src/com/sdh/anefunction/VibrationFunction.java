888ebb4026f7c189289584e01725f08978b86c11
신동환 <@SDH-PC>
2014-05-07 17:53:48 +0900

    매개변수로 전달받은 시간(밀리초)만큼 진동기능을 사용하게 하는 클래스

---
+++ AneFunction/src/com/sdh/anefunction/VibrationFunction.java
@@ -0,0 +1,49 @@
+package com.sdh.anefunction;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREInvalidObjectException;
+import com.adobe.fre.FREObject;
+import com.adobe.fre.FRETypeMismatchException;
+import com.adobe.fre.FREWrongThreadException;
+
+public class VibrationFunction extends Activity implements FREFunction{
+	
+	@Override
+	public FREObject call(FREContext arg0, FREObject[] arg1) {
+
+		int vibTime = 1000;
+		AneFunctionContext vibExtContext = (AneFunctionContext) arg0;
+		
+		Activity a = vibExtContext.getActivity();
+
+		vibExtContext.androidVibrator = (Vibrator) a.getSystemService(Context.VIBRATOR_SERVICE);
+		
+		Toast.makeText(arg0.getActivity(), "진동이다!!", Toast.LENGTH_LONG).show();
+		
+		try {
+			vibTime = arg1[0].getAsInt();
+			vibExtContext.androidVibrator.vibrate(vibTime);
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FRETypeMismatchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FREInvalidObjectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FREWrongThreadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
