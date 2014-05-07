efb304ed05ad1d1b1330bc35d1c252a311d0bbb5
신동환 <@SDH-PC>
2014-05-07 17:52:30 +0900

    ANE Context 클래스. 함수 사용 선언은 여기서..

---
+++ AneFunction/src/com/sdh/anefunction/AneFunctionContext.java
@@ -0,0 +1,36 @@
+package com.sdh.anefunction;
+ 
+import java.util.HashMap;
+import java.util.Map;
+ 
+
+import android.os.Vibrator;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+ 
+public class AneFunctionContext extends FREContext {
+ 
+	public Vibrator androidVibrator = null;
+
+	public AneFunctionContext()
+	{
+	}
+	
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+    	androidVibrator = null;
+    }
+ 
+    @Override
+    public Map<String, FREFunction> getFunctions() {
+        // TODO Auto-generated method stub
+        Map<String, FREFunction> map = new HashMap<String, FREFunction>();
+        map.put("toast", new ToastFunction());
+        map.put("vibration", new VibrationFunction());
+        map.put("deviceinfo", new DeviceInfoFunction());
+        return map;
+    }
+ 
+}
