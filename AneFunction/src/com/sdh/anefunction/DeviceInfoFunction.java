89d658326d20c706031d07e7e18bfd3c9e213f55
신동환 <@SDH-PC>
2014-05-08 11:24:56 +0900

    문자열의 내용을 비교하도록 변경, == -> equals

--- AneFunction/src/com/sdh/anefunction/DeviceInfoFunction.java
+++ AneFunction/src/com/sdh/anefunction/DeviceInfoFunction.java
@@ -2,39 +2,41 @@ package com.sdh.anefunction;
 
 import android.app.Activity;
 import android.os.Build;
+import android.widget.Toast;
 
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
 
-public class DeviceInfoFunction extends Activity implements FREFunction{
-	
+public class DeviceInfoFunction extends Activity implements FREFunction
+{
 	@Override
-	public FREObject call(FREContext arg0, FREObject[] arg1) {
+	public FREObject call(FREContext arg0, FREObject[] arg1) 
+	{
 		CharSequence message = "";
-            
+		
 		try
 		{
 			message = arg1[0].getAsString();
 			
 			FREObject obj = FREObject.newObject("Object", null);
 			
-			if     (message == "BOARD")           obj.setProperty("BOARD",           FREObject.newObject(Build.BOARD));
-			else if(message == "BRAND")           obj.setProperty("BRAND",           FREObject.newObject(Build.BRAND));
-			else if(message == "CPU_ABI")         obj.setProperty("CPU_ABI",         FREObject.newObject(Build.CPU_ABI));
-			else if(message == "DEVICE")          obj.setProperty("DEVICE",          FREObject.newObject(Build.DEVICE));
-			else if(message == "DISPLAY")         obj.setProperty("DISPLAY",         FREObject.newObject(Build.DISPLAY));
-			else if(message == "FINGERPRINT")     obj.setProperty("FINGERPRINT",     FREObject.newObject(Build.FINGERPRINT));
-			else if(message == "HOST")            obj.setProperty("HOST",            FREObject.newObject(Build.HOST));
-			else if(message == "ID")              obj.setProperty("ID",              FREObject.newObject(Build.ID));
-			else if(message == "MANUFACTURER")    obj.setProperty("MANUFACTURER",    FREObject.newObject(Build.MANUFACTURER));
-			else if(message == "MODEL")           obj.setProperty("MODEL",           FREObject.newObject(Build.MODEL));
-			else if(message == "PRODUCT")         obj.setProperty("PRODUCT",         FREObject.newObject(Build.PRODUCT));
-			else if(message == "TAGS")            obj.setProperty("TAGS",            FREObject.newObject(Build.TAGS));
-			else if(message == "TYPE")            obj.setProperty("TYPE",            FREObject.newObject(Build.TYPE));
-			else if(message == "USER")            obj.setProperty("USER",            FREObject.newObject(Build.USER));
-			else if(message == "VERSION.RELEASE") obj.setProperty("VERSION.RELEASE", FREObject.newObject(Build.VERSION.RELEASE));
-
+			if     (message.equals("BOARD"))          obj.setProperty("BOARD",           FREObject.newObject(Build.BOARD));
+			else if(message.equals("BRAND"))           obj.setProperty("BRAND",           FREObject.newObject(Build.BRAND));
+			else if(message.equals("CPU_ABI"))         obj.setProperty("CPU_ABI",         FREObject.newObject(Build.CPU_ABI));
+			else if(message.equals("DEVICE"))          obj.setProperty("DEVICE",          FREObject.newObject(Build.DEVICE));
+			else if(message.equals("DISPLAY"))         obj.setProperty("DISPLAY",         FREObject.newObject(Build.DISPLAY));
+			else if(message.equals("FINGERPRINT"))     obj.setProperty("FINGERPRINT",     FREObject.newObject(Build.FINGERPRINT));
+			else if(message.equals("HOST"))            obj.setProperty("HOST",            FREObject.newObject(Build.HOST));
+			else if(message.equals("ID"))              obj.setProperty("ID",              FREObject.newObject(Build.ID));
+			else if(message.equals("MANUFACTURER"))    obj.setProperty("MANUFACTURER",    FREObject.newObject(Build.MANUFACTURER));
+			else if(message.equals("MODEL"))           obj.setProperty("MODEL",           FREObject.newObject(Build.MODEL));
+			else if(message.equals("PRODUCT"))         obj.setProperty("PRODUCT",         FREObject.newObject(Build.PRODUCT));
+			else if(message.equals("TAGS"))            obj.setProperty("TAGS",            FREObject.newObject(Build.TAGS));
+			else if(message.equals("TYPE"))            obj.setProperty("TYPE",            FREObject.newObject(Build.TYPE));
+			else if(message.equals("USER"))            obj.setProperty("USER",            FREObject.newObject(Build.USER));
+			else if(message.equals("VERSION.RELEASE")) obj.setProperty("VERSION.RELEASE", FREObject.newObject(Build.VERSION.RELEASE));
+			
 			return obj;
 		} 
 		catch (Exception e)
