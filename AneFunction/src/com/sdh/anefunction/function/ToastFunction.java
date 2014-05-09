ac085a4bacb06ab624201d6b625abab419d41666
신동환 <@SDH-PC>
2014-05-09 16:17:33 +0900

    ToastFunction 주석작성, 공백 제거, 코딩스타일 통일, 토스트 지속시간 짧게로 변경

--- AneFunction/src/com/sdh/anefunction/function/ToastFunction.java
+++ AneFunction/src/com/sdh/anefunction/function/ToastFunction.java
@@ -1,37 +1,35 @@
 package com.sdh.anefunction.function;
  
 import android.widget.Toast;
- 
+
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
-import com.adobe.fre.FREInvalidObjectException;
 import com.adobe.fre.FREObject;
-import com.adobe.fre.FRETypeMismatchException;
-import com.adobe.fre.FREWrongThreadException;
  
-public class ToastFunction implements FREFunction {
- 
+/**
+ * 토스트를 출력하는 간단한 클래스입니다.
+ * @author 신동환
+ */
+public class ToastFunction implements FREFunction
+{
     @Override
-    public FREObject call(FREContext arg0, FREObject[] arg1) {
-        // TODO Auto-generated method stub
+    public FREObject call(FREContext arg0, FREObject[] arg1)
+    {
         CharSequence message = "";
-        try {
+        
+        try
+        {
+        	//출력할 메시지를 받아옵니다
             message = arg1[0].getAsString();
-        } catch (IllegalStateException e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        } catch (FRETypeMismatchException e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        } catch (FREInvalidObjectException e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        } catch (FREWrongThreadException e) {
-            // TODO: handle exception
-            e.printStackTrace();
         }
-        Toast.makeText(arg0.getActivity(), message, Toast.LENGTH_LONG).show();
+        catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+        
+        //토스트 출력
+        Toast.makeText(arg0.getActivity(), message, Toast.LENGTH_SHORT).show();
+        
         return null;
     }
- 
 }
