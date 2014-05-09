0715ab656a1b423d7c6496c1b1affe2ada9c1681
신동환 <@SDH-PC>
2014-05-09 16:08:17 +0900

    공백 제거 및 코딩 스타일 통일

--- AneFunction/src/com/sdh/anefunction/AneFunctionExtension.java
+++ AneFunction/src/com/sdh/anefunction/AneFunctionExtension.java
@@ -1,26 +1,22 @@
 package com.sdh.anefunction;
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREExtension;
- 
- 
-public class AneFunctionExtension implements FREExtension {
-	
+  
+public class AneFunctionExtension implements FREExtension
+{
     @Override
-    public FREContext createContext(String arg0) {
-        // TODO Auto-generated method stub
+    public FREContext createContext(String arg0)
+    {
         return new AneFunctionContext();
     }
  
     @Override
-    public void dispose() {
-        // TODO Auto-generated method stub
- 
+    public void dispose()
+    {
     }
  
     @Override
-    public void initialize() {
-        // TODO Auto-generated method stub
- 
+    public void initialize()
+    {
     }
- 
 }
