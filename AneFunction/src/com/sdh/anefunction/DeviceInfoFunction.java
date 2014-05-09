bbcade286cb9c49fec2529a401ac0cb5b5cae24a
신동환 <@SDH-PC>
2014-05-09 12:06:09 +0900

    Device 정보를 하나의 문자열로 반환하게 바꿈

--- AneFunction/src/com/sdh/anefunction/DeviceInfoFunction.java
+++ AneFunction/src/com/sdh/anefunction/DeviceInfoFunction.java
@@ -12,30 +12,26 @@ public class DeviceInfoFunction extends Activity implements FREFunction
 	@Override
 	public FREObject call(FREContext arg0, FREObject[] arg1) 
 	{
-		CharSequence message = "";
-		
+		String str =  "BOARD = "             + Build.BOARD        +
+                      "\nBRAND = "           + Build.BRAND        +
+                      "\nCPU_ABI = "         + Build.CPU_ABI      +
+                      "\nDEVICE = "          + Build.DEVICE       +
+                      "\nDISPLAY = "         + Build.DISPLAY      +
+                      "\nFINGERPRINT = "     + Build.FINGERPRINT  +
+                      "\nHOST = "            + Build.HOST         +
+                      "\nID = "              + Build.ID           +
+                      "\nMANUFACTURER = "    + Build.MANUFACTURER +
+                      "\nMODEL = "           + Build.MODEL        +
+                      "\nPRODUCT = "         + Build.PRODUCT      +
+                      "\nTAGS = "            + Build.TAGS         +
+                      "\nTYPE = "            + Build.TYPE         +
+                      "\nUSER = "            + Build.USER         +
+                      "\nVERSION.RELEASE = " + Build.VERSION.RELEASE;
 		try
 		{
-			message = arg1[0].getAsString();
-			
 			FREObject obj = FREObject.newObject("Object", null);
-			
-			if     (message.equals("BOARD"))           obj.setProperty("BOARD",           FREObject.newObject(Build.BOARD));
-			else if(message.equals("BRAND"))           obj.setProperty("BRAND",           FREObject.newObject(Build.BRAND));
-			else if(message.equals("CPU_ABI"))         obj.setProperty("CPU_ABI",         FREObject.newObject(Build.CPU_ABI));
-			else if(message.equals("DEVICE"))          obj.setProperty("DEVICE",          FREObject.newObject(Build.DEVICE));
-			else if(message.equals("DISPLAY"))         obj.setProperty("DISPLAY",         FREObject.newObject(Build.DISPLAY));
-			else if(message.equals("FINGERPRINT"))     obj.setProperty("FINGERPRINT",     FREObject.newObject(Build.FINGERPRINT));
-			else if(message.equals("HOST"))            obj.setProperty("HOST",            FREObject.newObject(Build.HOST));
-			else if(message.equals("ID"))              obj.setProperty("ID",              FREObject.newObject(Build.ID));
-			else if(message.equals("MANUFACTURER"))    obj.setProperty("MANUFACTURER",    FREObject.newObject(Build.MANUFACTURER));
-			else if(message.equals("MODEL"))           obj.setProperty("MODEL",           FREObject.newObject(Build.MODEL));
-			else if(message.equals("PRODUCT"))         obj.setProperty("PRODUCT",         FREObject.newObject(Build.PRODUCT));
-			else if(message.equals("TAGS"))            obj.setProperty("TAGS",            FREObject.newObject(Build.TAGS));
-			else if(message.equals("TYPE"))            obj.setProperty("TYPE",            FREObject.newObject(Build.TYPE));
-			else if(message.equals("USER"))            obj.setProperty("USER",            FREObject.newObject(Build.USER));
-			else if(message.equals("VERSION.RELEASE")) obj.setProperty("VERSION.RELEASE", FREObject.newObject(Build.VERSION.RELEASE));
-			
+			obj.setProperty("Info", FREObject.newObject(str));
+
 			return obj;
 		} 
 		catch (Exception e)
